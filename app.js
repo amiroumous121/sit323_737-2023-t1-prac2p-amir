ee7978dfed426f8a8686d0336a00f640b0acb43f
amer121 <109520654+amer121hacccer@users.noreply.github.com>
2023-03-09 21:25:48 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+// my first program for unit
+//console.log("hello");
+
+// var log = function (msg) {
+//   console.log("[log]:", msg);
+// };
+
+// log("hello world");
+// log("welcome to sit323/737");
+
+var adder = function (first, second) {
+  var sum = first + second;
+  return sum;
+};
+var log = function (msg) {
+  console.log("[log]:", msg);
+};
+log("hello world");
+log("welcome to sit323/737");
+log("the sum is " + adder(5, 6));
